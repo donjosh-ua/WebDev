8bd4609f2ddfaaa3a96b3420e7facad000f1b0b9
Oscariolo <oscariolopinop21@gmail.com>
2025-02-10 12:20:32 -0500

    OO-ADD-Added product model for django

--- BackendDjango/products/views.py
+++ BackendDjango/products/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from django.core import serializers
+from django.http import JsonResponse
 
 # Create your views here.
 
 def get_products(request):
-    return HttpResponse('Mis productos')
+    querySet = Product.objects.all().values('id','name','description','quantity')
+    data = list(querySet)
+    return JsonResponse(data,safe=False)
 
 def get_product(request, id):
     return HttpResponse(f'Producto escogido: {id}')
